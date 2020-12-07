66d08f6556b670d85f81eedb07922a77eb26391d
Shreyas <narahari.shreyas@gmail.com>
2020-12-08 01:14:43 +0530

    bilat filter

--- code/Detail_Transfer/main.m
+++ code/Detail_Transfer/main.m
@@ -7,7 +7,7 @@
 % joint_bilateral_filter(input, flash_input, 25, 0.001, 101);
 
 %% Import images
-ambient_input = im2double(imread('../../data/Compressed_Detail_Transfer/carpet_01_noflash.png'));
+ambient_input = im2double(imread('../../data/Detail_Transfer/carpet_00_flash.tif'));
 flash_input = im2double(imread('../../data/Compressed_Detail_Transfer/carpet_00_flash.png'));
 imshow(ambient_input);
 title('Input Ambient Image');
@@ -16,15 +16,15 @@ imshow(flash_input);
 title('Input Flash Image');
 
 %% Denoising using bilateral and joint bilateral filter
-A_base = bilateral_filter(ambient_input, 8, 0.07, 33);
+A_base = bilateral_filter(ambient_input, 25, 0.07, 101);
 figure;
 imshow(A_base);
 title('Bilateral Filter');
-imwrite(A_base,'../../output_images/compressed/carpet_noflash_bilateral.png');
-A_nr = joint_bilateral_filter(ambient_input, flash_input, 8, 0.001, 33);
-figure;
-imshow(A_nr);
-title('Joint Bilateral Filter');
-imwrite(A_base,'../../output_images/compressed/carpet_noflash_joint_bilateral.png');
+imwrite(A_base,'../../output_images/carpet_flash_bilateral.png');
+% A_nr = joint_bilateral_filter(ambient_input, flash_input, 8, 0.001, 33);
+% figure;
+% imshow(A_nr);
+% title('Joint Bilateral Filter');
+% imwrite(A_base,'../../output_images/compressed/carpet_noflash_joint_bilateral.png');
 
 %% Detail transfer
--- code/White_Balance/myMainScript.m
+++ code/White_Balance/myMainScript.m
@@ -3,5 +3,55 @@
 tic;
 %% Your code here
 
+flash = im2double(imread('../../data/White_Balance/potsWB_00_flash.jpg'));
+ambient = im2double(imread('../../data/White_Balance/potsWB_01_noflash.jpg'));
+% figure;
+% imshow(flash);
+figure;
+imshow(ambient);
+impixelinfo;
+
+flash_lin = my_linearization(flash);
+ambient_lin = my_linearization(ambient);
+
+delta = flash_lin - ambient_lin;
+
+C_p = zeros(size(ambient));
+
+C_p(:,:,1) = imdivide(ambient(:,:,1), delta);
+C_p(:,:,2) = imdivide(ambient(:,:,2), delta);
+C_p(:,:,3) = imdivide(ambient(:,:,3), delta);
+
+% ambient_sum = ambient(:,:,1) + ambient(:,:,2) + ambient(:,:,3);
+t1_r = 0.02*range(ambient(:,:,1), 'all');
+t1_g = 0.02*range(ambient(:,:,2), 'all');
+t1_b = 0.02*range(ambient(:,:,3), 'all');
+t2 = 0.02*range(delta, 'all');
+
+[h, w] = size(delta);
+
+disp(mean(C_p, 'all'));
+
+for i = 1:w
+    for j = 1:h
+    if ambient(j,i,1)<t1_r && ambient(j,i,2)<t1_g && ambient(j,i,3)<t1_b ...
+            || delta(j,i)<t2
+        C_p(j,i,:)= 0.0;
+    end
+    end
+end
+
+disp(mean(C_p, 'all'));
+
+c = mean(C_p, 'all');
+
+ambient_wb = contrast_streching(imdivide(ambient, c));
+
+% disp(max(ambient_wb,[],'all'));
+% disp(min(ambient_wb,[],'all'));
+
+figure;
+imshow(ambient_wb);
+impixelinfo;
 
 toc;
