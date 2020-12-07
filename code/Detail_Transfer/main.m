a3786b094030579d02da41e4bb11b62e0517da05
Raj Gite <rajgite1998@gmail.com>
2020-12-08 01:25:47 +0530

    Finished computing bilateral filter for original carpet image

--- code/Detail_Transfer/main.m
+++ code/Detail_Transfer/main.m
@@ -7,8 +7,8 @@
 % joint_bilateral_filter(input, flash_input, 25, 0.001, 101);
 
 %% Import images
-ambient_input = im2double(imread('../../data/Compressed_Detail_Transfer/carpet_01_noflash.png'));
-flash_input = im2double(imread('../../data/Compressed_Detail_Transfer/carpet_00_flash.png'));
+ambient_input = im2double(imread('../../data/Detail_Transfer/carpet_01_noflash.tif'));
+flash_input = im2double(imread('../../data/Detail_Transfer/carpet_00_flash.tif'));
 imshow(ambient_input);
 title('Input Ambient Image');
 figure;
@@ -16,15 +16,30 @@ imshow(flash_input);
 title('Input Flash Image');
 
 %% Denoising using bilateral and joint bilateral filter
-A_base = bilateral_filter(ambient_input, 8, 0.07, 33);
+A_base = bilateral_filter(ambient_input, 25, 0.07, 101);
 figure;
 imshow(A_base);
 title('Bilateral Filter');
-imwrite(A_base,'../../output_images/compressed/carpet_noflash_bilateral.png');
-A_nr = joint_bilateral_filter(ambient_input, flash_input, 8, 0.001, 33);
+imwrite(A_base,'../../output_images/carpet_noflash_bilateral.tif');
+A_nr = joint_bilateral_filter(ambient_input, flash_input, 25, 0.001, 101);
 figure;
 imshow(A_nr);
 title('Joint Bilateral Filter');
-imwrite(A_base,'../../output_images/compressed/carpet_noflash_joint_bilateral.png');
+imwrite(A_base,'../../output_images/carpet_noflash_joint_bilateral.tif');
 
 %% Detail transfer
+difference = rgb2gray(A_nr - A_base);
+difference = 1 - difference;
+difference = (difference - min(min(difference))) / (max(max(difference))- min(min(difference)));
+figure;
+imshow(difference);
+imwrite(difference, '../../output_images/carpet_difference.tif');
+F = flash_input;
+F_base = im2double(imread('../../output_images/carpet_flash_bilateral.tif'));
+F_detail = (F + 0.02) ./ (F_base + 0.02);
+
+
+
+
+
+
