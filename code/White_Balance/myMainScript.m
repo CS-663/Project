bbb5c848f90a3b9503a4b8f0191d1db2e19271e6
Shreyas <narahari.shreyas@gmail.com>
2020-12-08 23:41:13 +0530

    Added IMages

--- code/White_Balance/myMainScript.m
+++ code/White_Balance/myMainScript.m
@@ -3,8 +3,8 @@
 tic;
 %% Your code here
 
-flash = im2double(imread('../../data/White_Balance/potsWB_00_flash.jpg'));
-ambient = im2double(imread('../../data/White_Balance/potsWB_01_noflash.jpg'));
+flash = im2double(imread('../../data/Detail_Transfer/potsdetail_00_flash.tif'));
+ambient = im2double(imread('../../data/Detail_Transfer/potsdetail_03_our_result.tif'));
 % figure;
 % imshow(flash);
 figure;
@@ -15,7 +15,9 @@ impixelinfo;
 flash_lin = my_linearization(flash);
 ambient_lin = my_linearization(ambient);
 
-delta = flash_lin - ambient_lin;
+delta = (flash_lin - ambient_lin);
+% maximum = max(delta, [], 'all');
+% delta = delta./maximum;
 
 C_p = zeros(size(ambient));
 
@@ -23,22 +25,27 @@ C_p(:,:,1) = imdivide(ambient(:,:,1), delta);
 C_p(:,:,2) = imdivide(ambient(:,:,2), delta);
 C_p(:,:,3) = imdivide(ambient(:,:,3), delta);
 
-
-% ambient_sum = ambient(:,:,1) + ambient(:,:,2) + ambient(:,:,3);
+% ambient_sum = sqrt(ambient(:,:,1).^2 + ambient(:,:,2).^2 + ambient(:,:,3).^2);
 t1_r = 0.02*range(ambient(:,:,1), 'all');
 t1_g = 0.02*range(ambient(:,:,2), 'all');
 t1_b = 0.02*range(ambient(:,:,3), 'all');
+% t1 = 0.02*range(ambient_sum, 'all');
 t2 = 0.02*range(delta, 'all');
 
 [h, w] = size(delta);
 
+% sqrt(ambient(j,i,1).^2 + ambient(j,i,2).^2 + ambient(j,i,3).^2)<t1
+% ambient(j,i,1)<t1_r && ambient(j,i,2)<t1_g && ambient(j,i,3)<t1_b
+
 figure;
 imshow(C_p);
+title('Estimated ambient illumination');
 impixelinfo;
+
 
 for i = 1:w
     for j = 1:h
-    if ambient(j,i,1)<t1_r || ambient(j,i,2)<t1_g || ambient(j,i,3)<t1_b ...
+    if  ambient(j,i,1)<t1_r || ambient(j,i,2)<t1_g || ambient(j,i,3)<t1_b ...
             || delta(j,i)<t2
         C_p(j,i,:)= 0.0;
     end
@@ -47,12 +54,10 @@ end
 
 % C_p = contrast_streching(C_p);
 
-disp(max(C_p,[],'all'));
-disp(min(C_p,[],'all'));
+c_r = mean(nonzeros(C_p(:,:,1)), 'all');
+c_g = mean(nonzeros(C_p(:,:,2)), 'all');
+c_b = mean(nonzeros(C_p(:,:,3)), 'all');
 
-c_r = mean(C_p(:,:,1), 'all');
-c_g = mean(C_p(:,:,2), 'all');
-c_b = mean(C_p(:,:,3), 'all');
 
 ambient_wb = zeros(size(ambient));
 
@@ -65,6 +70,8 @@ figure;
 imshow(cat(3,patch*c_r,patch*c_g,patch*c_b));
 title('Estimated ambient illumination');
 
+disp(max(ambient_wb,[],'all'));
+disp(min(ambient_wb,[],'all'));
 
 figure;
 imshow(ambient_wb);
